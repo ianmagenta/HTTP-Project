f1cf3e14d21d94959e57201ab6ab350aaa51e345
Ian Magenta <ianmagenta@gmail.com>
2020-03-02 15:43:38 -0600

    Second commit

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const http = require('http');
 
 http.createServer (function (req, res) {
+    if (req.url === "/OK") {
+        console.log("Inbound 'OK' request being processed...");
+        res.writeHead(200);
+        res.end();
+    } else {
+        console.log("404: Page Not Found");
+        res.writeHead(404);
+        res.end();
+    }
     res.end();
 
 }).listen(3000, function(){
